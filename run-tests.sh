12a592ecb7a4e803ad638a1509784d072587b69e
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-03-04 14:28:07 +0000

    use upgrade

--- run-tests.sh
+++ run-tests.sh
@@ -3,14 +3,13 @@
 echo "Check if command exists"
 type ai-cli
 
+# both init and upgrade are needed to successfully use ai-cli run 
+
 echo "exec init"
 bash -x ai-cli init
 
-echo "exec help"
-bash -x ai-cli -h
-
-echo "exec status"
-bash -x ai-cli status
+echo "exec upgrade"
+bash -x ai-cli upgrade
 
 echo "exec run"
 ( cd tests/run_basic && bash -x ./exec_test.sh )
