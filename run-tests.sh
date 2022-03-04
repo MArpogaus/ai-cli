e13162a62b16afc1630d0bbacf09a7c7bb2c6530
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-03-04 15:01:22 +0000

    test

--- run-tests.sh
+++ run-tests.sh
@@ -7,9 +7,12 @@ type ai-cli
 
 echo "exec init"
 bash -x ai-cli init
+echo "exec init finished successfully"
 
 echo "exec upgrade"
 bash -x ai-cli upgrade
+echo "exec init finished successfully"
 
 echo "exec run"
 ( cd tests/run_basic && bash -x ./exec_test.sh )
+echo "exec run finished successfully"
