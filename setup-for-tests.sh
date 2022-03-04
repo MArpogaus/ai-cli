c178475f1aa7408bc9332d3f5fc8f3be23e84dcc
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-03-04 14:10:06 +0000

    setup for tests

--- setup-for-tests.sh
+++ setup-for-tests.sh
@@ -2,5 +2,8 @@
 
 echo "setting up prerequisites for testing"
 # start docker
+service docker start
 
 # setup git
+git config --global user.name "git-dummy-name"
+git config user.email "git-dummy@mail"
