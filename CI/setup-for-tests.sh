c0cf9be7fb6c8f8d6ab536602de487abdd9cbe9f
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-31 11:11:09 +0000

    add network proxy

--- CI/setup-for-tests.sh
+++ CI/setup-for-tests.sh
@@ -7,6 +7,9 @@ echo "setting up prerequisites for testing"
 # start docker
 sudo service docker start
 
+# setup network proxy
+docker network create proxy_net
+
 # setup git
 git config --global user.name "git-dummy-name"
 git config user.email "git-dummy@mail"
@@ -14,6 +17,5 @@ git config user.email "git-dummy@mail"
 # setup mlflow user
 sudo mkdir -p /usr/share/ai-cli/htpasswd/
 sudo htpasswd -Bb -c "/usr/share/ai-cli/htpasswd/${USER}-mlflow.localhost" ${USER} 1234
-
 
 echo "setup finished"
