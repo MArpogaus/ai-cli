42e2b437cd469e25aae8abd528b9f261d4299d9e
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-05-31 10:51:11 +0000

    make sure directory exists

--- CI/setup-for-tests.sh
+++ CI/setup-for-tests.sh
@@ -12,6 +12,7 @@ git config --global user.name "git-dummy-name"
 git config user.email "git-dummy@mail"
 
 # setup mlflow user
+sudo mkdir -p /usr/share/ai-cli/htpasswd/
 sudo htpasswd -Bb -c "/usr/share/ai-cli/htpasswd/dummy-mlflow.ai4grids.ei.htwg-konstanz.de" dummy 1234
 
 
