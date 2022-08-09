73bba5b10aba6c17e4ee091d60216a0b71b4cda9
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-08-09 23:03:48 +0000

    enable examples ci

--- CI/run-tests.sh
+++ CI/run-tests.sh
@@ -15,6 +15,6 @@ echo "start mlflow"
 bash -ex ai-cli start-server
 echo "start mlflow finished successfully"
 
-#echo "exec run"
-#( cd tests/run_basic && bash -x ./exec_test.sh )
-#echo "exec run finished successfully"
+echo "exec run"
+bash -ex ( cd examples/run_basic && bash -x ./ci-tests.sh )
+echo "exec run finished successfully"
