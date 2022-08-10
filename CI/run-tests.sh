d2994d15d6b88757179df352a781c052b65d298f
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2022-08-10 15:05:36 +0000

    also show content

--- CI/run-tests.sh
+++ CI/run-tests.sh
@@ -17,5 +17,6 @@ echo "CI: start mlflow finished successfully"
 
 echo "CI: exec run"
 bash -ex "pwd"
+bash -ex "ls -al"
 bash -ex "cd examples/basic && bash -x ./ci-tests.sh "
 echo "CI: exec run finished successfully"
