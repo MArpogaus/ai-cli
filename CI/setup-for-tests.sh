6e9858422da79433bc45aa67f094e27e0fda6047
MArpogaus <38564291+MArpogaus@users.noreply.github.com>
2022-09-02 09:12:10 +0000

    reads paths from config and creates MLflow dir

--- CI/setup-for-tests.sh
+++ CI/setup-for-tests.sh
@@ -14,9 +14,10 @@ docker network create proxy_net
 git config --global user.name "git-dummy-name"
 git config user.email "git-dummy@mail"
 
-# setup mlflow user
-sudo mkdir -p /usr/share/ai-cli/htpasswd/
-sudo htpasswd -Bb -c "/usr/share/ai-cli/htpasswd/${USER}-mlflow.localhost" ${USER} 1234
+# setup mlflow
+source /etc/ai-cli/config
+sudo mkdir -p $MLFLOW_DATA
+sudo htpasswd -Bb -c "$HTPASSWD_PATH/${USER}-mlflow.localhost" ${USER} 1234
 
 # install ts for timed outputs
 sudo apt-get install moreutils
