d82d1ecfad6e866db36526212906639cacfaa56d
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-02-13 17:50:03 +0000

    without dns

--- examples/basic/ci-tests.sh
+++ examples/basic/ci-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ai-cli start-server
 #ai-cli -n basic run .
-wget http://server:5000 && cat index.html
+wget http://0.0.0.0:5000 && cat index.html
 cat /etc/ai-cli/config
 docker network ls
