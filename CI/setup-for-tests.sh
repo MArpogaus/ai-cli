40009dac708de65cb508284cba27236e67c9683a
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-03-06 21:14:45 +0000

    fix permission?

--- CI/setup-for-tests.sh
+++ CI/setup-for-tests.sh
@@ -15,9 +15,14 @@ git config --global user.name "git-dummy-name"
 git config user.email "git-dummy@mail"
 
 # setup mlflow
+echo "setting up mlflow environment"
 source /etc/ai-cli/config
 sudo mkdir -p $MLFLOW_DATA
+sudo mkdir -p /mlruns
+sudo chmod 777 /mlruns
+sudo chmod 777 $MLFLOW_DATA
 sudo htpasswd -Bb -c "$HTPASSWD_PATH/${USER}-mlflow.localhost" ${USER} 1234
+
 
 # install ts for timed outputs
 sudo apt-get install moreutils
